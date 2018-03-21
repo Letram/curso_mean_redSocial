323d44dfb009669a711d8ea7d0754fa0eb9d0ed5
Carlos Martel Lamas <charliky_ro@hotmail.es>
2018-03-21 16:34:43 +0000

    First file of the project.
    Started a social network MEAN app.

---
+++ api/index.js
@@ -0,0 +1,12 @@
+//use of the new JavaScript standards such as lambda functions and so on.
+'use strict'
+
+//connection to a mongoDB database via mongoose module
+var mongoose = require('mongoose');
+//we are connectiong to the database using promises.
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/curso_mean_red_social')
+		//if we connect to our db properly, show it on the console.
+		.then(()=>console.log("Conexión a DB OK."))
+		//if not, show the error.
+		.catch(err => console.log(err));
