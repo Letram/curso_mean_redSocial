8d8b520e54fbe105250bc851b6158bc73c484040
Carlos Martel Lamas <carlos.martel105@alu.ulpgc.es>
2018-03-22 10:32:17 +0000

    Save user
    Save user function implemented.

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,5 +1,6 @@
 'use strict'
 
+var bcrypt = require('bcrypt-nodejs');
 //capitalizaed so we know it is a model
 var User = require('../models/user');
 
@@ -16,8 +17,49 @@ function pruebas(request, response){
 	});
 }
 
+function saveUser(request, response){
+	var params = request.body;
+	var user = new User();
+
+	if(params.name && params.surname && params.nick && params.email && params.password){
+		user.name = params.name;
+		user.surname = params.surname;
+		user.nick = params.nick;
+		user.email = params.email;
+
+		user.role = 'ROLE_USER';
+		user.image = null;
+
+		bcrypt.hash(params.password, null, null, function (error, passwordHashed){
+			user.password = passwordHashed;
+
+			user.save(function(error, userStored){
+				if(error)
+					return response.status(500).send({
+						message:"Error al guardar el usuario"
+					});
+				if(userStored){
+					return response.status(200).send({
+						user:userStored
+					});
+				}else{
+					return response.status(404).send({
+						message:"No se ha registrado el usuario."
+					});
+				}
+			});
+		});
+
+	}else{
+		//if any of the params are missing we have to tell them they have to do it right
+		response.status(200).send({
+			message:"Rellena todos los datos necesarios para registrar el usuario."
+		});
+	}
+}
 //we are exporting "home" and "pruebas" functions so we can use them from other classes if we import this.
 module.exports = {
 	home,
-	pruebas
+	pruebas,
+	saveUser
 }
--- api/routes/user.js
+++ api/routes/user.js
@@ -7,5 +7,5 @@ var api = express.Router();
 
 api.get('/home', userController.home);
 api.get('/pruebas', userController.pruebas);
-
+api.post('/register', userController.saveUser)
 module.exports = api;
